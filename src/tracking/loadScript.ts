6d902ec7b1656330c8e9a6b5923166f4caf6cddc
bozdoz <howaboutben@gmail.com>
2021-05-08 18:06:26 -0300

    adds sentry in client

---
+++ src/tracking/loadScript.ts
@@ -0,0 +1,20 @@
+/** It adds a script tag to the DOM */
+const loadScript = (
+  src: string,
+  options: Partial<Writable<HTMLScriptElement>> = {}
+) => {
+  const script = document.createElement('script');
+  const firstScript = document.getElementsByTagName('script')[0];
+  script.async = true;
+  script.src = src;
+
+  // eslint-disable-next-line guard-for-in
+  for (const key in options) {
+    const typedKey = key as keyof typeof options;
+    (script as any)[typedKey] = options[typedKey];
+  }
+
+  firstScript.parentNode!.insertBefore(script, firstScript);
+};
+
+export default loadScript;
--- src/tracking/sentry.ts
+++ src/tracking/sentry.ts
@@ -1,18 +1,36 @@
-import * as Sentry from '@sentry/browser';
-import { Integrations } from '@sentry/tracing';
+import loadScript from './loadScript';
+import waitFor from './waitFor';
+
+// TODO: create a queue to store calls before sentry loaded
+window.sentry = {
+  addBreadcrumb() {},
+  captureException() {},
+  captureMessage() {},
+  setExtras() {},
+};
 
 if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn:
-      'https://ce393e73e1e74437accc9886f98abb7d@o69899.ingest.sentry.io/149608',
+  loadScript('https://browser.sentry-cdn.com/6.2.5/bundle.min.js', {
+    integrity:
+      'sha384-+0tgGyP4idWu9/NA6Jbmnj3SApxIg65/GR1zo3qSieRNyzmmDQ/5a1Yu6mfmUw+v',
+    crossOrigin: 'anonymous',
+  });
 
-    // To set your release version
-    release: `typewritesomething@${process.env.npm_package_version}`,
-    integrations: [new Integrations.BrowserTracing()],
+  waitFor(() => !!window.Sentry).then(() => {
+    window.Sentry.init({
+      dsn:
+        'https://ce393e73e1e74437accc9886f98abb7d@o69899.ingest.sentry.io/149608',
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+      // To set your release version
+      release: `typewritesomething@${process.env.npm_package_version}`,
+      integrations: [new window.Integrations.BrowserTracing()],
+
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+    });
+
+    window.sentry = window.Sentry;
   });
 }
