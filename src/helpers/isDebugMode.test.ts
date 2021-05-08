478cd2a7c6da9a16041a88b5be2415eaaa8fa1d4
bozdoz <howaboutben@gmail.com>
2021-05-08 18:05:48 -0300

    fix some tests; add some tests

---
+++ src/helpers/isDebugMode.test.ts
@@ -0,0 +1,40 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+describe('isDebugMode', () => {
+  it('is not debug mode by default', () => {
+    expect(window.location.search).toBeFalsy();
+
+    jest.isolateModules(() => {
+      const isDebugMode = require('./isDebugMode').default;
+      expect(isDebugMode()).toBeFalsy();
+    });
+  });
+
+  it('could be debug mode', () => {
+    const windowSpy = jest.spyOn(global, 'window', 'get');
+    const search = '?debug=true';
+
+    windowSpy.mockImplementationOnce(() => {
+      const mockWindow: any = {
+        ...window,
+        location: {
+          ...window.location,
+        },
+      };
+
+      Object.defineProperty(mockWindow.location, 'search', {
+        get() {
+          return search;
+        },
+      });
+
+      return mockWindow;
+    });
+
+    jest.isolateModules(() => {
+      const isDebugMode = require('./isDebugMode').default;
+
+      expect(isDebugMode()).toBeTruthy();
+    });
+  });
+});
