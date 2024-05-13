75836bbeb1901487808a97700908ce4a987bec0a
bozdoz <howaboutben@gmail.com>
2024-05-13 19:52:11 -0300

    import sentry again

--- src/tracking/sentry.ts
+++ src/tracking/sentry.ts
@@ -1,36 +1,24 @@
-import loadScript from './loadScript';
-import waitFor from './waitFor';
-
-// TODO: create a queue to store calls before sentry loaded
-window.sentry = {
-  addBreadcrumb() {},
-  captureException() {},
-  captureMessage() {},
-  setExtras() {},
-};
+import * as Sentry from '@sentry/browser';
 
 if (process.env.NODE_ENV === 'production') {
-  loadScript('https://browser.sentry-cdn.com/6.3.6/bundle.tracing.min.js', {
-    integrity:
-      'sha384-i2Yvbs5RFObbcbRhpGsmI2wLv+I4jxpEET1WzR/KWxLdwZMtP8E1M0PROnrwe2gF',
-    crossOrigin: 'anonymous',
-  });
+  Sentry.init({
+    dsn: 'https://ce393e73e1e74437accc9886f98abb7d@o69899.ingest.us.sentry.io/149608',
 
-  waitFor(() => !!window.Sentry).then(() => {
-    window.Sentry.init({
-      dsn:
-        'https://ce393e73e1e74437accc9886f98abb7d@o69899.ingest.sentry.io/149608',
+    // To set your release version
+    release: `typewritesomething@${process.env.npm_package_version}`,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
 
-      // To set your release version
-      release: `typewritesomething@${process.env.npm_package_version}`,
-      integrations: [new window.Sentry.Integrations.BrowserTracing()],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.4,
 
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for performance monitoring.
-      // We recommend adjusting this value in production
-      tracesSampleRate: 1.0,
-    });
-
-    window.sentry = window.Sentry;
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
   });
 }
